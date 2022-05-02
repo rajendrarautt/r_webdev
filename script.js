8f1c7d2980c86f82fd252a76fd4aa2e00ec2b23d
Dell <Dell@RAJENDRA>
2022-05-02 18:19:01 +0545

    commit for the branch rajan

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ $(document).ready(function()
     {
         console.log(alert("MF are you ready?"));
     });
+    $('#home').click(function(){
+        alert("Are you ready MF?");
+    });
+    $('#fucking_f_heading').click(function(){
+        $(this).hide();
+        $('#fucking_f_headings').Show();
+    });
 });
+
+// $(document).ready(function(){
+//     $('#home').click(function(){
+//         alert("Are you ready MF?");
+//     });
+//     $('#fucking_f_heading').click(function(){
+//         $(this).hide();
+//         $('#fucking_f_headings').Show();
+//     });
+// });
+    function createTableforABC()
+{
+    let b = "";
+    for(const a = 1; a<=8; a++)
+    {
+        alert("The First Fucker is you");
+        b += "The First Fucker is you " + a + "<br>";
+        document.body.innerHTML= b;
+    }
+
+}
